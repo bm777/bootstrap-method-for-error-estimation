9091c7f93e292d945ac2b27cd466ab4dfe77193b
Bayangmbe Mounmo <bayangp0@gmail.com>
2021-01-31 03:11:04 +0100

    Added pick, calculate_mean, calculate_median function to the class

--- estimator.py
+++ estimator.py
@@ -1,6 +1,48 @@
 import time
+import random
+from random import randint
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++
+class Estimator():
+    def __init__(self):
+        self.set = self.dataset()
+        self.mean_median = [[], []]
 
 
-print("+++++++++++++++++++++++++++++++++++++++++")
 
-print("+++++++++++++++++++++++++++++++++++++++++")
+    def __del__(self):
+        del self
+    def __str__(self):
+        return "CLass of error estimator using bootstrap method."
+
+
+    def dataset(self, samples=40):
+        return [randint(30, 170) for s in range(samples)]
+
+    # step 2
+    def pick(self, samples=40):
+        return sorted([self.set[randint(0,s)] for s in range(samples)])
+
+    def calculate_mean(self, resampled):
+        return mean(resampled)
+
+    def calculate_median(self, resampled):
+        if len(resampled) % 2 == 1:
+            return resampled[resampled//2]
+        elif len(resampled) % 2 == 0:
+            if resampled[resampled//2] != resampled[1+resampled//2]:
+                return resampled[resampled//2] * 0.5 + resampled[1+resampled//2] * 0.5
+            else:
+                return resampled[resampled//2]
+
+if __name__ == '__main__':
+    estim = Estimator()
+
+    print("+++++++++++++++++++++++++++++++++++++++++")
+
+    print("step [1] := DATASET GENERATION")
+    print("-dataset : ",estim.set, "\n-size : ",len(estim.set))
+
+    print("\nstep [2,3] := ")
+    print("-picked up : ",estim.pick())
+    print("+++++++++++++++++++++++++++++++++++++++++")
